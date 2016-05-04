7e65c0504d6323a34ce59ea42d2999b04e6f44cb
Heber Andrade <heberandrade@Hebers-MacBook-Pro.local>
2016-05-04 09:42:48 +0200

    initial commit

---
+++ BloggApp/ViewController.swift
@@ -0,0 +1,102 @@
+//
+//  ViewController.swift
+//  BloggApp
+//
+//  Created by Heber Andrade on 08/04/16.
+//  Copyright © 2016 Heber Andrade. All rights reserved.
+//
+
+import UIKit
+import WatsonDeveloperCloud
+
+class ViewController: UIViewController {
+    @IBOutlet weak var printLabelLocotions: UILabel!
+    @IBOutlet weak var printHashtagLabel: UILabel!
+    
+    @IBAction func fastLocation(sender: AnyObject) {
+      
+        let requestURL = NSMutableURLRequest(URL: NSURL(string: "http://hashtaggenerator201.eu-gb.mybluemix.net/events?lat=55.6118668&lng=12.9772628&distance=1000")!)
+        
+        let response = NSHTTPURLResponse()
+      
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithRequest(requestURL, completionHandler: {data, response, error -> Void in
+            let error = error
+            let response = response
+            let data = data
+            if  data != nil && error == nil {
+                let res = response as! NSHTTPURLResponse!
+                if (res.statusCode >= 200 && res.statusCode < 300) {
+                    do {
+                        let jsonData:AnyObject? = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.AllowFragments) as? NSDictionary
+                        
+                        if let counter = jsonData!["entity"] as? [AnyObject] {
+                            let type1 = counter[0]
+                            let tag = counter[1]
+                            
+                            
+                        }
+                        
+                        
+                        
+                        print("Success")
+                    } catch let error as NSError {
+                        print("Failed To load")
+                    } catch {
+                        // Something else happened.
+                        // Insert your domain, code, etc. when constructing the error.
+                        
+                    }
+                } else {
+                    print("Connection Error")
+                }
+                
+            } else {
+                print("Error")
+            }
+        })
+        task.resume()
+    
+        
+        func httpGet(requestURL: NSURLRequest, callback: (String?, NSError?) -> Void) {
+            let task = NSURLSession.sharedSession().dataTaskWithRequest(requestURL) { data, respond, error in
+                guard error == nil && data != nil else {
+                    callback(nil, error)
+                    return
+                }
+                
+                callback(String(data: data!, encoding: NSUTF8StringEncoding), nil)
+            }
+            task.resume()
+        }
+        
+        
+        httpGet(requestURL) { string, error in
+            guard error == nil && string != nil else {
+                print(error?.localizedDescription)
+                return
+            }
+            
+            print(string!)
+        }
+    
+        
+
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
